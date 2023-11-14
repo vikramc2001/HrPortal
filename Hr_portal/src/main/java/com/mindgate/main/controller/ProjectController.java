5b3c711d3504828468ca1731873fa84ae2538d5d
MGSCHN-AKSHAYA R <MGSCHN-AKSHAYA R@MGSCHN-LAP-357>
2023-11-14 12:27:21 +0530

    cross origin

--- Hr_portal/src/main/java/com/mindgate/main/controller/ProjectController.java
+++ Hr_portal/src/main/java/com/mindgate/main/controller/ProjectController.java
@@ -3,6 +3,7 @@ package com.mindgate.main.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.mindgate.main.domain.Project;
 import com.mindgate.main.service.ProjectServiceInterface;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("projectapi")
 public class ProjectController {
 	@Autowired
